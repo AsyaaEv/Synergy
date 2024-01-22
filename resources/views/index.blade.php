31a10f3db6ae7a132a112d1072a908a7ec87825f
AsyaaEv <asyaaeval@gmail.com>
2024-01-22 14:25:42 +0700

    Add bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,11 @@
     <style>
     </style>
     @livewireStyles
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
     @yield('content')
     @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- resources/views/livewire/pplg/guru.blade.php
+++ resources/views/livewire/pplg/guru.blade.php
@@ -1,3 +1,17 @@
-<div class="w-full h-screen flex justify-center items-center flex-col bg-black">
-  
+<div class="w-full h-screen flex justify-center items-center border-2">
+    <form class="">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+    </form>
+
 </div>
