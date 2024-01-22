fc99872e7ee77d8e8d979b4af6dd677764f2ffec
AsyaaEv <asyaaeval@gmail.com>
2024-01-22 11:09:09 +0700

    3 com

---
+++ app/Livewire/Pplg/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pplg;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('livewire.pplg.home');
+    }
+}
