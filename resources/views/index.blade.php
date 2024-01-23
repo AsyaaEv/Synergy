c3106aec854b168c70c30a6103d738e43e2ebfe8
AsyaaEv <asyaaeval@gmail.com>
2024-01-23 08:18:06 +0700

    add phosphor icon cdn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,7 @@
     <style>
     </style>
     @livewireStyles
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
--- resources/views/livewire/pplg/guru.blade.php
+++ resources/views/livewire/pplg/guru.blade.php
@@ -1,5 +1,61 @@
 <div class="w-full h-screen flex justify-center items-center">
-    <div>
+    <div class="w-full border-2 h-full container bg-gray-100 gap-[10px] ">
+        <div class="w-full h-auto border-2 my-[2rem] flex">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Add Guru
+              </button>
+              
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#ID</th>
+                <th scope="col">Foto</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Kode</th>
+                <th scope="col">Mapel</th>
+                <th scope="col">Nip</th>
+                <th scope="col">Email</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+                @forelse ($guru as $item)
+                <tr>
+                  <th scope="row">{{$item->id}}</th>
+                  <td><img src="{{Storage::url($item->foto)}}" alt="" class="w-[2rem] h-[2rem]"></td>
+                  <td>{{$item->nama}}</td>
+                  <td>00</td>
+                  <td>{{$item->mapel}}</td>
+                  <td>{{$item->nip}}</td>
+                  <td>{{$item->email}}</td>
+                  <td></td>
+                </tr>
+                @empty
+                    <div class="">Not FOund</div>
+                @endforelse
+            </tbody>
+          </table>
+    </div>
+    {{-- <div>
         @if (session()->has('msg'))
             <div class="alert @if (!empty(session('alert'))) alert-{{ session('alert') }} @else d-none @endif"">
                 @if (!empty(session('alert')))
@@ -32,6 +88,6 @@
         <div class="input-group mb-3">
             <button class="btn btn-primary">Submit</button>
         </div>
-    </form>
+    </form> --}}
 
 </div>
