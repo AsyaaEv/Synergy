299ed507ae68611e68301d26b2d07353c992edc3
itsmeeBazz <saktiawanabas@gmail.com>
2024-01-24 13:56:53 +0700

    visi-misi

--- resources/views/components/visi.blade.php
+++ resources/views/components/visi.blade.php
@@ -1,14 +1,9 @@
 <section class="w-auto h-auto py-8">
-    <div class="container flex items-center flex-col">
-        <div class="w-full flex border-2">
-            <div class="border border-black flex text-center rounded-lg justify-center h-9 w-32">
-                <p class="font-semibold my-1">Visi dan Misi</p>
-            </div>
-            <div class="w-full h-auto flex justify-end">
-                <img src="{{Storage::url('assets/pplg/img/background.png')}}" class="object-cover h-full w-[15rem]" alt="">
-            </div>
+    <div class="container flex flex-col">
+            <p class="font-semibold my-1 border border-black">Visi dan Misi</p>
+            <img src="{{Storage::url('assets/pplg/img/background.png')}}" class="object-cover h-full w-[15rem]" alt="">
         </div>
-        <div class="w-full flex-col h-full items-center flex justify-end border-2 border-black">
+        <div class="w-[40%] flex-col h-full flex border-2 border-black">
             <h1 class="capitalize font-bold">Visi</h1>
             <p class="font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
         </div>
