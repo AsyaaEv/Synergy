2adfe5e8e1bd4ec52f4d4cecdb37c68eeb37d2f7
itsmeeBazz <saktiawanabas@gmail.com>
2024-01-24 19:31:29 +0700

    visi-misi

--- resources/views/components/visi.blade.php
+++ resources/views/components/visi.blade.php
@@ -1,11 +1,15 @@
 <section class="w-auto h-auto py-8">
     <div class="container flex flex-col">
-            <p class="font-semibold my-1 border border-black">Visi dan Misi</p>
-            <img src="{{Storage::url('assets/pplg/img/background.png')}}" class="object-cover h-full w-[15rem]" alt="">
-        </div>
+        <h1
+            class="border-[3px] w-[12rem] rounded-[10px] border-black p-[21.7px] inline-block m-0 text-[32px] text-black">
+            About Us
+        </h1>
+        <img src="{{ Storage::url('assets/pplg/img/background.png') }}" class="object-cover h-full w-[15rem]"
+            alt="">
         <div class="w-[40%] flex-col h-full flex border-2 border-black">
             <h1 class="capitalize font-bold">Visi</h1>
-            <p class="font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+            <p class="font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
         </div>
     </div>
 </section>
