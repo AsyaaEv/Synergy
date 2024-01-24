5e3d60898b010eb51a681ea0899972abac0d1bd6
RGBXY <derrenamadeo@gmail.com>
2024-01-24 14:51:39 +0700

    about-com-3

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-<section class="w-full h-screen flex flex-col py-20 items-center justify-center object-cover bg-center" style="background-image: url({{Storage::url('assets/pplg/img/background.png')}})">
-    <div class="jumbotron w-auto items-center h-auto border-2 container">
-        <div class="w-[90%]">
-            <h1 class="text-2xl font-semibold text-[#f78704]">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
-=======
 <section class="w-full h-screen flex flex-col py-20 items-center justify-center object-cover bg-left-top" style="background-image: url({{Storage::url('assets/pplg/img/background.png')}})">
     <div class="jumbotron w-full items-center h-auto container">
         <div class="md:w-[27%] w-full h-auto">
             <h1 class="text-2xl font-semibold text-[#f78704]">Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
->>>>>>> fbd85e25e8c4d81388c7132b02ce77a89a1aaf97
         </div>
         <div class="md:w-[43%] h-auto">
             <p class=" font-semibold text-lg text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
