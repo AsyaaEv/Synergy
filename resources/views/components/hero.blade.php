584e06a049471fa035c57999630970d92777e9be
itsmeeBazz <saktiawanabas@gmail.com>
2024-01-24 13:29:20 +0700

    hero-com-2

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,10 +1,10 @@
-<section class="w-full h-screen flex flex-col py-20 items-center justify-center object-cover bg-[]" style="background-image: url({{Storage::url('assets/pplg/img/background.png')}})">
-    <div class="jumbotron w-auto items-center h-auto border-2 container">
-        <div class="w-[90%]">
-            <h1 class="text-2xl font-semibold text-[#f78704]">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+<section class="w-full h-screen flex flex-col py-20 items-center justify-center object-cover bg-left-top" style="background-image: url({{Storage::url('assets/pplg/img/background.png')}})">
+    <div class="jumbotron w-full items-center h-auto container">
+        <div class="md:w-[27%] w-full h-auto">
+            <h1 class="text-2xl font-semibold text-[#f78704]">Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
         </div>
-        <div class="w-[80%]">
-            <p class=" font-semibold text-lg text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea velit ducimus, delectus numquam aperiam reprehenderit. Dignissimos nesciunt ad quaerat quia maxime quisquam earum consequuntur veritatis.</p>
+        <div class="md:w-[43%] h-auto">
+            <p class=" font-semibold text-lg text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
         </div>
         <a class="btn btn-primary btn-sm " href="#" role="button">Learn more</a>
     </div>
--- resources/views/livewire/pplg/home.blade.php
+++ resources/views/livewire/pplg/home.blade.php
@@ -1,5 +1,6 @@
 <div>
     @include('components.navbar')
     @include('components.hero')
+    @include('components.visi')
     {{-- The Master doesn't talk, he acts. --}}
 </div>
