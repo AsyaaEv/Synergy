aca8d47966d173e0a5011a0c56d0623156c7fa07
itsmeeBazz <saktiawanabas@gmail.com>
2024-01-25 07:38:48 +0700

    visi-misi1

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,11 +1,11 @@
 <section class="w-full h-screen flex flex-col py-20 items-center justify-center object-cover bg-left-top"
     style="background-image: url({{ Storage::url('assets/pplg/img/background.png') }})">
     <div class="jumbotron w-full items-center h-auto container">
-        <div class="md:w-[27%] w-full h-auto">
-            <h1 class="text-2xl font-semibold text-[#f78704]">Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
+        <div class="md:w-[40%] w-full h-auto">
+            <h1 class="text-[35px] font-semibold text-[#f78704]">Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
         </div>
-        <div class="md:w-[43%] h-auto">
-            <p class=" font-normal text-lg text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        <div class="md:w-[43%] h-auto mt-3">
+            <p class="font-normal text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
         </div>
         {{-- <a class="btn btn-primary btn-sm rounded" href="#" role="button">Learn more</a> --}}
