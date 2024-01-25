4906c2de3210fd643a1014a22f2a2eb6b95d97d8
itsmeeBazz <saktiawanabas@gmail.com>
2024-01-25 09:55:06 +0700

    visi-misi3

--- resources/views/components/visi.blade.php
+++ resources/views/components/visi.blade.php
@@ -2,10 +2,10 @@
     style=" background-size: cover; background-image: url({{ Storage::url('assets/pplg/img/bgVisiMisi.jpg') }})">
     <div class="container ">
         <h1
-            class="border-[3px] w-[10.5rem] rounded-[10px] border-black mb-4 md:mb-0 p-[10px] inline-block m-0 text-[25px] text-blborder-black">
+            class="border-[3px] w-[12rem] rounded-[10px] border-black mb-4 md:mb-0 p-[10px] inline-block m-0 text-[25px] text-blborder-black">
             Visi dan Misi</h1>
         <div class=" w-full flex h-auto justify-between items-center flex-col-reverse md:flex-row">
-            <div class="md:w-[50%] w-full flex-col mt-5 mb-3 md:mb-5 md:mt-5 h-full flex ">
+            <div class="md:w-[50%] w-full flex-col mb-3 md:mb-5  h-full flex ">
                 <h1 class="capitalize font-bold text-black">Visi</h1>
                 <p class="text-black text-[20px] font-bold mt-4 ">Lorem ipsum dolor sit amet, consectetur
                     adipiscing elit, sed
@@ -13,7 +13,7 @@
                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
             <img src="{{ Storage::url('assets/pplg/img/background.png')}}"
-                class="object-cover w-[25rem] h-[25rem] rounded-tl-[3rem] rounded-tr-[3rem] rounded-br-[3rem] md:-mt-[4.5rem]"
+                class="object-cover w-[25rem] h-[25rem] mb-5 rounded-tl-[3rem] rounded-tr-[3rem] rounded-br-[3rem] md:-mt-[4.5rem]"
                 alt="">
         </div>
         <div class=" w-full flex h-auto justify-between items-center flex-col-reverse  md:flex-row-reverse">
