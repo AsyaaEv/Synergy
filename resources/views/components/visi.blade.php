625cd294d7f2903a52309ce129c8fe81676c1deb
itsmeeBazz <saktiawanabas@gmail.com>
2024-02-01 07:20:14 +0700

    programKerja

--- resources/views/components/visi.blade.php
+++ resources/views/components/visi.blade.php
@@ -5,7 +5,7 @@
             class="border-[1px] w-[12rem] text-hitam font-bold rounded-[10px] border-black mb-4 md:mb-0 p-[10px] inline-block m-0 text-[25px] text-blborder-black">
             Visi dan Misi</h1>
         <div class=" w-full flex h-auto justify-between items-center flex-col-reverse md:flex-row">
-            <div class="md:w-[50%] w-[90%] flex-col mb-3 md:mb-5  h-full flex ">
+            <div class="w-[90%] md:w-[60%] lg:w-[50%] flex-col mb-3 lg:mb-5  h-full flex ">
                 <h1 class="capitalize font-bold text-hitam">Visi</h1>
                 <p class="text-hitam text-[20px] font-bold mt-2 ">Lorem ipsum dolor sit amet, consectetur
                     adipiscing elit, sed
@@ -13,19 +13,19 @@
                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
             <img src="{{ Storage::url('assets/pplg/img/background.png')}}"
-                class="object-cover w-[25rem] h-[25rem] mb-5 rounded-tl-[3rem] rounded-tr-[3rem] rounded-br-[3rem] md:-mt-[4.5rem]"
+                class="object-cover w-[25rem] md:w-[20rem] lg:w-[25rem] md:h-[20rem] lg:h-[25rem] h-[25rem] mb-5 rounded-tl-[3rem] rounded-tr-[3rem] rounded-br-[3rem] md:mt-8 lg:-mt-[4.5rem]"
                 alt="">
         </div>
-        <div class="w-full flex h-auto justify-between items-center flex-col-reverse  md:flex-row-reverse">
-            <div class="md:w-[50%] w-[90%] flex-col mt-5 mb-3 md:mb-0 md:mt-0 h-full flex ">
-                <h1 class="capitalize font-bold text-hitam">Misi</h1>
+        <div class=" w-full flex h-auto md:gap-6 lg:justify-between items-center flex-col-reverse md:flex-row-reverse">
+            <div class="w-[90%] md:w-[60%] lg:w-[50%] flex-col mb-3 md:mb-5  h-full flex ">
+                <h1 class="capitalize font-bold text-hitam">Visi</h1>
                 <p class="text-hitam text-[20px] font-bold mt-2 ">Lorem ipsum dolor sit amet, consectetur
                     adipiscing elit, sed
                     do eiusmod tempor
                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
             <img src="{{ Storage::url('assets/pplg/img/background.png')}}"
-                class="object-cover w-[25rem] h-[25rem] rounded-tl-[3rem] rounded-tr-[3rem] rounded-br-[3rem] md:-mt-[4.5rem]"
+                class="object-cover w-[25rem] md:w-[20rem] lg:w-[25rem] md:h-[20rem] lg:h-[25rem] h-[25rem] mb-5 rounded-tl-[3rem] rounded-tr-[3rem] rounded-br-[3rem] md:-mt-[4.5rem]"
                 alt="">
         </div>
     </div>
